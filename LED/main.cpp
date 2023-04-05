db103e7983a6b7485753229295bd7a7d1b970e7f
Ryoga Washizawa <ryg.wsza@gmail.com>
2023-04-05 13:38:05 +0900

    Add 'LED'

---
+++ LED/main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+
+#define BLINKING_RATE 500ms
+
+
+int main() {
+    DigitalOut led(LED1);
+
+    while (true) {
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
