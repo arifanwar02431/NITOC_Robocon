f3587480f4425dedd9e22b7cc1f1fff0d99199b8
Ryoga Washizawa <ryg.wsza@gmail.com>
2023-04-17 13:29:13 +0900

    Add comment to step1.cpp

--- practice_capsule/step1.cpp
+++ practice_capsule/step1.cpp
@@ -6,8 +6,8 @@ PwmOut p(D3);
 int main() {
     p.period_ms(5);
     while(true) {
-        dig = 1;
-        p = 0.3;
+        dig = 1;        // モータの回転方向(0 or 1)
+        p = 0.3;        // モータの回転速度(max: 1.0)
     }
     return 0;
 }
